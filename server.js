7ad69e0f2afb9abb542df96d693f67eb04a57ea1
yousuf <yoyousuf67@gmail.com>
2017-02-04 23:43:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/articleone',function(req,res){
+   res.send('article one requested and will be served here'); 
+});
+app.get('/articletwo',function(req,res){
+   res.send('article two requested and will be served here'); 
+});
+app.get('/articlethree',function(req,res){
+   res.send('article three requested and will be served here'); 
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));

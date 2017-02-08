83e0823a43dbb9af4a03077581fa1aa50025b450
yousuf <yoyousuf67@gmail.com>
2017-02-08 19:14:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/articleone', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'articleone.html'));
 });
 
+app.get('/articletwo', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articletwo.html'));
+});
+
+app.get('/articlethree', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'articlethree.html'));
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });

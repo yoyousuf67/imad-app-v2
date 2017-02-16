c70f56a539853faea885d20e9e26eeae958fbb93
yousuf <yoyousuf67@gmail.com>
2017-02-16 22:09:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,16 @@
 console.log('Loaded!');
+//change the text
 var element= document.getElementById('maintext');
 element.innerHTML='new value';
+
+//move the image
+var img=document.getElementById('madi');
+var marginleft=0;
+function moveRight(){
+    marginleft=marginleft+1;
+    img.style.marginleft+"px"
+}
+madi.onclick=function(){
+  img.style.marginleft='100px';
+  var interval=setInterval(moveRight,50);
+};

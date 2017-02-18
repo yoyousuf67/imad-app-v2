f45256154515b09a6200d712c7f9a41738da747c
yousuf <yoyousuf67@gmail.com>
2017-02-18 19:47:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -43,7 +43,7 @@ request.onreadystatechange=function(){
      
          var list='';
          for(var i=0;i<names.length;i++){
-             list += '<li>'+ names[i]+ '<li>';
+             list += '<li>'+ names[i]+ '</li>';
          }
          var ul=document.getElementById('namelist');
          ul.innerHTML=list;

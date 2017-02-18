56ac80dcc96f67a7461132bcef972693f7b6027c
yousuf <yoyousuf67@gmail.com>
2017-02-18 13:25:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,25 @@
-console.log('Loaded!');
-//change the text
-/*var element= document.getElementById('maintext');
-element.innerHTML='new value';*/
+//counter code
+var button=document.getElementById('counter');
+var counter=0;
 
-//move the image
-var img=document.getElementById('madi');
-var marginleft=0;
-function moveRight(){
-    marginleft=marginleft+1;
-    img.style.marginleft + 'px';
-}
-img.onclick=function(){
-  var interval=setInterval(moveRight,50);
-};
+button.onclick=function(){
+    
+//create a request object
+var request=new XMLHttpRequest();
+
+//capture respinse and store variable
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      //take action
+      if(requset.status==200){
+      var counter= request.responseText;
+      var span=document.getElementById('count');
+    span.innerHTML=counter.toString();
+      }
+  }  
+  //not done ignore it
+}; 
+//make a request
+reqest.open('GET','http://yoyousuf67.imad.hasura-app.io/counter',true);
+request.send(null);
+ };

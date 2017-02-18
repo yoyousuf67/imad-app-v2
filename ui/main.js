305c86bf4d7ffba2fb83678c462d21b6cf28aef5
yousuf <yoyousuf67@gmail.com>
2017-02-18 19:11:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,15 +12,22 @@ request.onreadystatechange=function(){
   if(request.readyState==XMLHttpRequest.DONE){
       //take action
       if(request.status==200){
-      var counter= request.responseText;
-      var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+         //capture a list of names and render it as a list
+         var names=request.responseText;
+         names=JSON.parse(names);
+     
+         var list='';
+         for(var i=0;i<names.length;i++){
+             list += '<li>'+ names[i]+ '<li>';
+         }
+         var ul=document.getElementById('namelist');
+         ul.innerHTML=list;
       }
   }  
   //not done ignore it
 }; 
 //make a request
-request.open('GET','http://yoyousuf67.imad.hasura-app.io/counter',true);
+request.open('GET','http://yoyousuf67.imad.hasura-app.io/submit-name?name='+name,true);
 request.send(null);
  };
  
@@ -30,13 +37,4 @@ request.send(null);
  var submit=document.getElementById('submit_btn');
  submit.onclick=function(){
      //make a request to the server and send the name
-     //capture a list of names and render it as a list
-     var names=['name1','name2','name3','name4'];
- 
-     var list='';
-     for(var i=0;i<names.length;i++){
-         list += '<li>'+ names[i]+ '<li>';
-     }
-     var ul=document.getElementById('namelist');
-     ul.innerHTML=list;
  };

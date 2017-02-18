9750ff3fbdebe169782d3db06d4283e69e02d715
yousuf <yoyousuf67@gmail.com>
2017-02-18 19:54:13 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -39,7 +39,7 @@ request.onreadystatechange=function(){
       if(request.status==200){
             //capture a list of names and render it as a list
         var names=request.responseText;
-        name=JSON.parse(names);
+        names=JSON.parse(names);
          var list='';
          for(var i=0;i<names.length;i++){
              list += '<li>'+names[i]+ '</li>';

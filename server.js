66f048d78705e60a7b3120351ec94ed0e372fda3
yousuf <yoyousuf67@gmail.com>
2017-02-25 21:28:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+var config={
+    user:'yoyousuf67',
+    database:'yoyousuf67',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD//environment variable
+}
 
 var app = express();
 app.use(morgan('combined'));
@@ -74,6 +82,22 @@ var htmlTemplate=` <html>
 
     return htmlTemplate;
 }
+
+var pool = new Pool(config);
+app.get('/test-db',function (req,res){
+    //make a slect request
+    //return a response with the result
+
+    pool.query('SELECT * FROM test',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+
+});
+});
 var counter=0;
 app.get('/counter',function(req,res){
    counter=counter+1;
@@ -83,7 +107,6 @@ app.get('/counter',function(req,res){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-
 var names= [];
 app.get('/submit-name',function(req,res){
    //Get the name from the request object

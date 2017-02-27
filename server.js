08acaf7ef05e9d71a6d3feae504da1b30217c2e9
yousuf <yoyousuf67@gmail.com>
2017-02-27 21:33:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ articleThree:{ title:'Article Three',
 };
 function createTemplate (data){
     var title=data.title;
-    var date=data.title;
+    var date=data.date;
     var heading=data.heading;
     var content=data.content;
 var htmlTemplate=` <html>

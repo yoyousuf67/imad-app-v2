49d7b5cbdd27e13398d05f1078c4e6fd7a0b0de1
yousuf <yoyousuf67@gmail.com>
2017-02-27 20:09:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -118,11 +118,24 @@ app.get('/submit-name',function(req,res){
    res.send(JSON.stringify(names));
 });
 
-app.get('/:articleName', function (req, res) {
+app.get('/article/:articleName', function (req, res) {
     //articleName=articleone
     //articles[articleName]=content object for articleOne
     var articleName= req.params.articleName;
- res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title="+ req.params.articleName, function(err, result){
+        if(err){
+            res.status(500).send(err.toString());
+        }else{
+            if(result.rows.length===0){
+                res.status(404).send('Article Not Found');
+            
+            }else
+            {
+                var articleData=result.rows[0];
+                res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
 
 app.get('/articletwo', function (req, res) {

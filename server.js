f3ea093046a39a17f68dbb2e10d8c4f132d28902
yousuf <yoyousuf67@gmail.com>
2017-03-01 12:25:20 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
+var crypto=require('crypto');
 var config={
     user:'yoyousuf67',
     database:'yoyousuf67',
@@ -82,6 +83,17 @@ var htmlTemplate=` <html>
 
     return htmlTemplate;
 }
+
+
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input.salt,10000,512,'sha512');
+    return hashed.toString('hex');//output is in bytes 
+}
+app.get('/hash/:input',function(req,res){ 
+    var hashedString=hash(req.params.input, 'this-is-string');
+    res.send(hashedString);
+})
+
 
 var pool = new Pool(config);
 app.get('/test-db',function (req,res){

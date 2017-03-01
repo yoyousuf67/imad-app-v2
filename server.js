a7ea4e7752ddaf97ec400208aeee910c1285e6a3
yousuf <yoyousuf67@gmail.com>
2017-03-01 13:48:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ var username=req.body.username;
 var password=req.body.password;
     var salt=crypto.randomBytes(128).toString('hex');
     var doString=hash(password,salt);
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,doString],function(err,result){
+    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         }

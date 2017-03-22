f99390f109e92d39e7a04ad57f3d0c489a1f6d82
yousuf <yoyousuf67@gmail.com>
2017-03-22 12:10:21 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,4 @@
-//counter code
+/*//counter code
 var button=document.getElementById('counter');
 var counter=0;
 
@@ -22,9 +22,9 @@ request.onreadystatechange=function(){
 //make a request
 request.open('GET','http://yoyousuf67.imad.hasura-app.io/counter',true);
 request.send(null);
- };
+ };*/
  
- //submit name
+ //submit username,pasword to login
 
  var submit=document.getElementById('submit_btn');
  submit.onclick=function(){
@@ -38,23 +38,32 @@ request.onreadystatechange=function(){
       //take action
       if(request.status==200){
             //capture a list of names and render it as a list
-        var names=request.responseText;
+       /* var names=request.responseText;
         names=JSON.parse(names);
          var list='';
          for(var i=0;i<names.length;i++){
              list += '<li>'+names[i]+ '</li>';
          }
          var ul=document.getElementById('namelist');
-         ul.innerHTML=list;
+         ul.innerHTML=list;*/
+         alert('Logged in Successfully');
+      }else if (request.status===403){alert('Username/password is incorrect');
       }
-  }  
+      else if(request.status===500){
+              alert('Something went wrong on server');
+              
+          }
+      }
   //not done ignore it
 }; 
- var nameInput=document.getElementById('name');
- var name= nameInput.value;
+ var username=document.getElementById('username').value;
+ var password=document.getElementById('password').value;
+ console.log(username);
+ console.log(password);
+ //var name= nameInput.value;
 //make a request
-request.open('GET','http://yoyousuf67.imad.hasura-app.io/submit-name?name='+name,true);
-request.send(null);
+request.open('POST','http://yoyousuf67.imad.hasura-app.io/submit-name?name='+name,true);
+request.send(JSON.stringify({username: username, password:password}));
 
      //make a request to the server and send the name
      

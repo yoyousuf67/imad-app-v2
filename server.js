afd49dab72bbf9719b1356463d8a15428891a502
yousuf <yoyousuf67@gmail.com>
2017-03-22 11:48:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -113,6 +113,31 @@ app.post('/create-user',function(req,res){
     });
 });
 
+app.post('/login', function(req,res){
+     var username=req.body.username;
+    var password=req.body.password;
+
+    pool.query('SELECT * FROM "user" WHERE username=$1 VALUES = $1',[username],function(err,result){
+        if (err){
+        res.status(500).send(err.toString());
+    } else 
+        {
+           if(result.rows.length===0){
+               res.send(403).send('username/password invalid');
+               
+           }
+           else{
+             var dbString=result.rows[0].password;
+             var salt=dbString.split('$')[2];
+             var hashedPassword=hash(password,salt);
+             if(hashedPassword===dbString){
+                 res.send('credentials are correct');
+             } else{
+               res.send(403).send('username/password invalid');
+        }}}
+    });
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function (req,res){
     //make a slect request

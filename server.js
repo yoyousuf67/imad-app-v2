2b1a13f47c808372e324abb4b83fa9a45339a214
yousuf <yoyousuf67@gmail.com>
2017-03-22 10:47:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,7 +87,7 @@ var htmlTemplate=` <html>
 
 function hash(input,salt){
     var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return hashed.toString('hex');//output is in bytes 
+    return["pbkdf2","1000",salt, hashed.toString('hex')].join($);//output is in bytes 
 }
 app.get('/hash/:input',function(req,res){ 
     var hashedString=hash(req.params.input, 'this-is-string');

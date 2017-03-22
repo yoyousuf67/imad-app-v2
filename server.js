277e89b0aa8d99b5e1a5bbe201d4bcd752d805a2
yousuf <yoyousuf67@gmail.com>
2017-03-22 11:03:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
+app.use(bodyParser.json());
 
 /*var articles={
 articleOne:{
@@ -94,6 +95,22 @@ app.get('/hash/:input',function(req,res){
     res.send(hashedString);
 });
 
+app.post('/create-user',function(req,res){
+    var username=req.body.username;
+    var password=req.body.password;
+    var salt=crypto.getRandomByte(128).toString('hex');
+    var dbString=hash(password,salt);
+
+    pool.query('INSERT INTO "user"  (username,password) VALUES ($1,$2)',[username,dbString],function(err,result){
+        if (err){
+        res.status(500).send(err.toString());
+    } else 
+        {
+            res.send('user successfully created:'+username);
+        }
+        
+    });
+});
 
 var pool = new Pool(config);
 app.get('/test-db',function (req,res){

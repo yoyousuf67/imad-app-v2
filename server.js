3d93821a0165359f10a65a3b3eccc50bd53498f1
yousuf <yoyousuf67@gmail.com>
2017-03-22 14:17:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -157,6 +157,8 @@ app.get('/check-login',function(req,res){
    if(req.session && req.session.auth && req.session.auth.userId){
        res.send('You are logged in:'+req.session.auth.userId.toString());
    } 
+   else{ 
+   res.send('you are not logged in');}
 });
 
 app.get('/logout',function(req,res){

a55536a4899bff9a623a0616e50f374ecb578acd
yousuf <yoyousuf67@gmail.com>
2017-03-27 14:24:30 +0530

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -13,6 +13,7 @@ request.onreadystatechange=function(){
       if(request.status==200){
             
          alert('User Created Successfully');
+         
       }else if (request.status===403){alert('Username/password is incorrect');
       }
       else if(request.status===500){
@@ -28,13 +29,12 @@ request.onreadystatechange=function(){
  console.log(password1);
  //var name= nameInput.value;
 //make a request
-if(username1===""||password1===""){alert("Username/password is incorrect")}
-else{
+
 request.open('POST','http://yoyousuf67.imad.hasura-app.io/create-user',true);
 
 request.setRequestHeader('Content-type','application/json');
 request.send(JSON.stringify({username: username1, password:password1}));
-}
+
 
      //make a request to the server and send the name
      

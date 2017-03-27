b92e96781eee397746f70352c52f3f2b0133be0d
yousuf <yoyousuf67@gmail.com>
2017-03-27 13:26:16 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -237,6 +237,10 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/ui/register.js', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'register.js'));
+});
+
 app.get('/ui/register.html', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'register.html'));
 });

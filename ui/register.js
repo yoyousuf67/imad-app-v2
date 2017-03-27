beef7dab0cbaa3e6dc4fbc92f3f23300e41c65bb
yousuf <yoyousuf67@gmail.com>
2017-03-27 13:58:58 +0530

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -1,5 +1,16 @@
+ 
 
-var submit1=document.getElementById('submit2');
+        function validateForm() {
+            var x = document.forms["myForm"]["username"].value;
+    var y= document.forms["myForm"]["password"].value
+    if (x === ""||y==="") {
+        alert("Name/Password must be filled out");
+        return false;
+    }
+            else {return true;}
+        }
+
+var submit1=document.getElementById('submit1');
  submit1.onclick=function(){
      
      //create a request object
@@ -7,6 +18,7 @@ var request=new XMLHttpRequest();
 
 //capture response and store variable
 request.onreadystatechange=function(){
+    if (validateForm()){
   if(request.readyState==XMLHttpRequest.DONE){
       //take action
       if(request.status==200){
@@ -16,18 +28,19 @@ request.onreadystatechange=function(){
               alert('Something went wrong on server');
               
           }
-      }
+     } 
+        else {alert('Invalid Username/Password');}
+        
+    }
  }; 
- var username=document.getElementById('username').value;
- var password=document.getElementById('password').value;
- console.log(username);
- console.log(password);
+ console.log(x);
+ console.log(y);
  //var name= nameInput.value;
 //make a request
 request.open('POST','http://yoyousuf67.imad.hasura-app.io/create-user',true);
 
 request.setRequestHeader('Content-type','application/json');
-request.send(JSON.stringify({username: username, password:password}));
+request.send(JSON.stringify({username: x, password:y}));
 
      //make a request to the server and send the name
      

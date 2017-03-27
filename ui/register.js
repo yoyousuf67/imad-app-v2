7b18448a942dd9c6b93b065ce6f60a9d5563a605
yousuf <yoyousuf67@gmail.com>
2017-03-27 13:20:04 +0530

    Update register.js

--- ui/register.js
+++ ui/register.js
@@ -1,1 +1,19 @@
 
+var submit1=document.getElementById('submit2');
+ submit1.onclick=function(){
+     
+     //create a request object
+var request=new XMLHttpRequest();
+
+//capture response and store variable
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      //take action
+      if(request.status==200){
+         alert('User Created Successfully');
+      }
+      else if(request.status===500){
+              alert('Something went wrong on server');
+              
+          }
+      }

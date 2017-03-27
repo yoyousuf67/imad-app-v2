984a6a458676251e1f58ae06b9d481e1bc21168d
yousuf <yoyousuf67@gmail.com>
2017-03-27 14:11:50 +0530

    [imad-console] Updates ui/register.js

--- ui/register.js
+++ ui/register.js
@@ -1,46 +1,38 @@
  
 
-        function validateForm() {
-            var x = document.forms["myForm"]["username"].value;
-    var y= document.forms["myForm"]["password"].value
-    if (x === ""||y==="") {
-        alert("Name/Password must be filled out");
-        return false;
-    }
-            else {return true;}
-        }
 
-var submit1=document.getElementById('submit1');
- submit1.onclick=function(){
+var submit=document.getElementById('submit1');
+ submit.onclick=function(){
      
      //create a request object
 var request=new XMLHttpRequest();
-
+if(username1===""||password1===""){request.status=403;}
 //capture response and store variable
 request.onreadystatechange=function(){
-    if (validateForm()){
   if(request.readyState==XMLHttpRequest.DONE){
       //take action
       if(request.status==200){
+            
          alert('User Created Successfully');
+      }else if (request.status===403){alert('Username/password is incorrect');
       }
       else if(request.status===500){
               alert('Something went wrong on server');
               
           }
-     } 
-        else {alert('Invalid Username/Password');}
-        
-    }
- }; 
- console.log(x);
- console.log(y);
+      }
+  //not done ignore it
+}; 
+ var username1=document.getElementById('username1').value;
+ var password1=document.getElementById('password1').value;
+ console.log(username1);
+ console.log(password1);
  //var name= nameInput.value;
 //make a request
-request.open('POST','http://yoyousuf67.imad.hasura-app.io/create-user',true);
+request.open('POST','http://yoyousuf67.imad.hasura-app.io/login',true);
 
 request.setRequestHeader('Content-type','application/json');
-request.send(JSON.stringify({username: x, password:y}));
+request.send(JSON.stringify({username: username1, password:password1}));
 
      //make a request to the server and send the name
      

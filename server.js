c7c82aed4c07e89f617cbcac72b7b149e1b6f4a5
yousuf <yoyousuf67@gmail.com>
2017-03-29 22:19:02 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -116,7 +116,7 @@ app.post('/create-user',function(req,res){
         res.status(500).send(err.toString());
     } 
     });
-     pool.query('INSERT INTO "details" (username,name,dob,gender) VALUES ($1,$2,$3,$4)',[name,dob],function(err,result){
+     pool.query('INSERT INTO "details" (username,name,dob,gender) VALUES ($1,$2,$3,$4)',[username,name,dob,gender],function(err,result){
         if (err){
         res.status(500).send(err.toString());
     } else 

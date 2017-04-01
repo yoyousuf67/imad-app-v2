2b53c45b6dbdc75a298e6768984528b84d9bad83
yousuf <yoyousuf67@gmail.com>
2017-04-01 16:02:54 +0530

    [imad-console] Updates ui/logout.js

--- ui/logout.js
+++ ui/logout.js
@@ -1,1 +1,18 @@
 
+var submit=document.getElementById('logout');
+ submit.onclick=function(){
+     
+     //create a request object
+var request=new XMLHttpRequest();
+//capture response and store variable
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      //take action
+      if(request.status==200){
+            
+         alert('Logged Out Successfully');
+          window.open('/',"_self");
+         
+      }
+      else{alert('Some error occurred');
+}}}};

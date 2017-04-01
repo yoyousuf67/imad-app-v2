266050a4557a69324c151db5ae3e40cb89ea0d10
yousuf <yoyousuf67@gmail.com>
2017-04-01 11:43:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,8 +3,6 @@ var morgan = require('morgan');
 var path = require('path');
 var Pool=require('pg').Pool;
 var crypto=require('crypto');
-var bodyParser=require('body-parser');
-var session=require('express-session');
 var config={
     user:'yoyousuf67',
     database:'yoyousuf67',
@@ -15,12 +13,6 @@ var config={
 
 var app = express();
 app.use(morgan('combined'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({
-    secret: 'someRandomSecretValue',
-    cookie:{maxAge:1000*60*60*24*30}
-}));
 
 /*var articles={
 articleOne:{
@@ -93,91 +85,15 @@ var htmlTemplate=` <html>
 }
 
 
-function hash(input,salt){
-    var hashed=crypto.pbkdf2Sync(input,salt,10000,512,'sha512');
-    return["pbkdf2","10000",salt, hashed.toString('hex')].join('$');//output is in bytes 
+function hash(input){
+    var hashed=crypto.pbkdf2Sync(input.salt,10000,512,'sha512');
+    return hashed.toString('hex');//output is in bytes 
 }
 app.get('/hash/:input',function(req,res){ 
     var hashedString=hash(req.params.input, 'this-is-string');
     res.send(hashedString);
-});
+})
 
-app.post('/create-user',function(req,res){
-    var username=req.body.username;
-    var password=req.body.password;
-    var dob=req.body.dob;
-    var name=req.body.name;
-    var gender=req.body.gender;
-    var salt=crypto.randomBytes(128).toString('hex');
-    var dbString=hash(password,salt);
-
-    pool.query('INSERT INTO "user" (username,password) VALUES ($1,$2)', [username,dbString],function(err,result){
-        if (err){
-        res.status(500).send(err.toString());
-    } else 
-        {
-            res.send('user successfully created:'+username);
-        }
-    });
-   /* 
-   pool.query('INSERT INTO "details" (username,name,dob,gender) VALUES ($1,$2,$3,$4)',[username,name,dob,gender],function(err,result){
-        if (err){
-        res.status(500).send(err.toString());
-    } else 
-        {
-            res.send('user successfully created:'+username);
-        }
-        
-    });*/
-});
-
-app.post('/login', function(req,res){
-     var username=req.body.username;
-    var password=req.body.password;
-
-    pool.query('SELECT * FROM "user" WHERE username= $1',[username],function(err,result){
-        if (err){
-        res.status(500).send(err.toString());
-    } else 
-        {
-           if(result.rows.length===0){
-               res.send(403).send('username/password invalid');
-               
-           }
-           else{
-             var dbString=result.rows[0].password;
-             var salt=dbString.split('$')[2];
-             var hashedPassword=hash(password,salt);
-             if(hashedPassword===dbString){
-                 
-                 //set the session
-                 req.session.auth={userId:result.rows[0].id};
-                 //set cookie with session id
-                 //internally on server side it maps sesion id to an object
-                 //object contains value of auth 
-                 
-                 //auth contains {userId}
-                 res.send('credentials are correct');
-                 
-                 
-             } else{
-               res.send(403).send('username/password invalid');
-        }}}
-    });
-});
-
-app.get('/check-login',function(req,res){
-   if(req.session && req.session.auth && req.session.auth.userId){
-       res.send('You are logged in:'+req.session.auth.userId.toString());
-   } 
-   else{ 
-   res.send('you are not logged in');}
-});
-
-app.get('/logout',function(req,res){
-    delete req.session.auth;
-    res.send('Logged out');
-});
 
 var pool = new Pool(config);
 app.get('/test-db',function (req,res){
@@ -250,24 +166,8 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
-app.get('/ui/register.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'register.js'));
-});
-
-app.get('/ui/register.html', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'register.html'));
-});
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
-});
-
-app.get('/ui/hospital.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'hospital.png'));
-});
-
-app.get('/ui/back.jpg', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'back.jpg'));
 });
 
 

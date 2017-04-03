8e2b98132a2fda18168260493c015913cb6d618e
yousuf <yoyousuf67@gmail.com>
2017-04-03 23:22:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.get('/check-login',function(req,res){
 
 app.get('/logout',function(req,res){
     delete req.session.auth;
-    res.send('Logged out');
+    res.status(200).send('Logged out');
 });
 
 var pool = new Pool(config);

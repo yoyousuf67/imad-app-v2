48799079cefc605bf4b3dfc04de767f70871f5af
yousuf <yoyousuf67@gmail.com>
2017-04-03 22:21:52 +0530

    [imad-console] Updates ui/report.js

--- ui/report.js
+++ ui/report.js
@@ -5,8 +5,15 @@ submit.onclick=function(){
 request.onreadystatechange=function(){
   if(request.readyState==XMLHttpRequest.DONE){
       if(request.status==200){
+          
+         
+     var request1=new XMLHttpRequest();
+request1.onreadystatechange=function(){
+  if(request1.readyState==XMLHttpRequest.DONE){
+      if(request1.status==200){
       alert("logged out succcessfully");
        window.open('/',"_self");
+      }}};
       }else if (request.status===403){alert('Error detected');
       }
       else if(request.status===500){
@@ -15,6 +22,7 @@ request.onreadystatechange=function(){
           }
       }
 };
-}; 
-      request.open('GET','http://yoyousuf67.imad.hasura-app.io/logout',true);
+ 
+      request.open('GET','http://yoyousuf67.imad.hasura-app.io/check-login',true);
       request.send(null);
+};

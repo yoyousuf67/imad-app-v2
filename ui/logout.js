076c333b3cfec4dfcacbbcb350b792f697918133
yousuf <yoyousuf67@gmail.com>
2017-04-03 21:53:06 +0530

    [imad-console] Updates ui/logout.js

--- ui/logout.js
+++ ui/logout.js
@@ -5,9 +5,14 @@ submit.onclick=function(){
 request.onreadystatechange=function(){
   if(request.readyState==XMLHttpRequest.DONE){
       if(request.status==200){
-           request.open('GET','http://yoyousuf67.imad.hasura-app.io/logout',true);
+           var request1=new XMLHttpRequest();
+request1.onreadystatechange=function(){
+  if(request1.readyState==XMLHttpRequest.DONE){
+      if(request1.status==200){
          alert('Logged out Successfully');
           window.open('/',"_self");
+      }}};
+      request1.open('GET','http://yoyousuf67.imad.hasura-app.io/logout',true);
       }else if (request.status===403){alert('Error detected');
       }
       else if(request.status===500){

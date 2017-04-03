e3f5e18a0192b1c6d025ab46a7922d2d090dfa7d
yousuf <yoyousuf67@gmail.com>
2017-04-03 19:34:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ articleThree:{ title:'Article Three',
             This is the content of my third article
             </p> `}
 };*/
-/*function createTemplate (data){
+function createTemplate (data){
     var title=data.title;
     var date=data.date;
     var heading=data.heading;
@@ -88,12 +88,6 @@ var htmlTemplate=` <html>
 </html>
  `;
 
-    return htmlTemplate;
-}*/
-
-function createTemplate(data){
-    var text=data.text;
-    var htmlTemplate='$text';
     return htmlTemplate;
 }
 
@@ -153,15 +147,7 @@ app.post('/login', function(req,res){
                  //object contains value of auth 
                  
                  //auth contains {userId}
-                 var articleName= req.params.text;
-    pool.query("SELECT * FROM test WHERE title= $1",[req.params.text], function(err, result){
-        
-            {
-                var articleData=result.rows[0];
-                res.send(createTemplate(articleData));
-            }
-    });
-                // res.send('credentials are correct');
+                 res.send('credentials are correct');
                  
                  
              } else{

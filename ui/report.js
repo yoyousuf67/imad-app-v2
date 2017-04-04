c2ea1b0123f3db71c100d44aa1ab470f4257b727
yousuf <yoyousuf67@gmail.com>
2017-04-04 20:26:53 +0530

    [imad-console] Updates ui/report.js

--- ui/report.js
+++ ui/report.js
@@ -21,3 +21,74 @@ request.onreadystatechange=function(){
       request.open('GET','http://yoyousuf67.imad.hasura-app.io/logout',true);
       request.send(null);
 };
+
+
+
+var submit=document.getElementById('doctor');
+submit.onclick=function(){
+     
+     var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      if(request.status==200){
+       var dbdetail= request.responseText;
+      var span=document.getElementById('details');
+    span.innerHTML=dbdetail.toString();
+ }
+      else if (request.status===403){alert('Error detected');
+      }
+      else if(request.status===500){
+              alert('Something went wrong on server');
+              
+          }
+      }
+};
+      console.log('request sent');
+      request.open('GET','http://yoyousuf67.imad.hasura-app.io/doctor',true);
+};
+
+var submit=document.getElementById('patients');
+submit.onclick=function(){
+     
+     var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      if(request.status==200){
+       var dbdetail= request.responseText;
+      var span=document.getElementById('details');
+    span.innerHTML=dbdetail.toString();
+ }
+      else if (request.status===403){alert('Error detected');
+      }
+      else if(request.status===500){
+              alert('Something went wrong on server');
+              
+          }
+      }
+};
+      console.log('request sent');
+      request.open('GET','http://yoyousuf67.imad.hasura-app.io/doctor',true);
+};
+
+var submit=document.getElementById('rd');
+submit.onclick=function(){
+     
+     var request=new XMLHttpRequest();
+request.onreadystatechange=function(){
+  if(request.readyState==XMLHttpRequest.DONE){
+      if(request.status==200){
+       var dbdetail= request.responseText;
+      var span=document.getElementById('details');
+    span.innerHTML=dbdetail.toString();
+ }
+      else if (request.status===403){alert('Error detected');
+      }
+      else if(request.status===500){
+              alert('Something went wrong on server');
+              
+          }
+      }
+};
+      console.log('request sent');
+      request.open('GET','http://yoyousuf67.imad.hasura-app.io/doctor',true);
+};

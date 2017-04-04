03170be35448a45f1a5369d02b0282b6297eb36b
yousuf <yoyousuf67@gmail.com>
2017-04-04 21:18:10 +0530

    [imad-console] Updates ui/report.js

--- ui/report.js
+++ ui/report.js
@@ -45,7 +45,7 @@ request.onreadystatechange=function(){
 };
       console.log('request sent');
       request.open('GET','http://yoyousuf67.imad.hasura-app.io/doctor',true);
-};}
+};};
 
 var submit2=document.getElementById('patients');
 submit2.onclick=function(){
